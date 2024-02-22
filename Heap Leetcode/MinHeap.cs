f79ff24449eeb4b2914eb8d37a696ee696dec959
hima.jose@mintgroup.net <hima.jose@mintgroup.net>
2024-02-22 11:10:19 +0000

    Add project files.

---
+++ Heap Leetcode/MinHeap.cs
@@ -0,0 +1,71 @@
+﻿// See https://aka.ms/new-console-template for more information
+public class MinHeap
+{
+    public List<int> Heap { get; set; } = new List<int>();
+
+    public void Add(int x)
+    {
+        Heap.Add(x);
+        HeapifyUp(Heap.Count - 1);
+    }
+
+    public int Pop()
+    {
+        if (Heap.Count == 0) throw new InvalidOperationException("Heap is empty.");
+
+        int value = Heap[0];
+        Heap[0] = Heap[Heap.Count - 1];
+        Heap.RemoveAt(Heap.Count - 1);
+        if (Heap.Count > 0)
+        {
+            HeapifyDown(0);
+        }
+
+        return value;
+
+    }
+
+    private void HeapifyDown(int index)
+    {
+        int minIndex = index;
+        int leftIndex = 2 * index + 1;
+        int rightIndex = 2 * index + 2;
+        if (leftIndex < Heap.Count && Heap[minIndex] > Heap[leftIndex])
+        {
+
+            minIndex = leftIndex;
+        }
+        if (rightIndex < Heap.Count && Heap[minIndex] > Heap[rightIndex])
+        {
+
+            minIndex = rightIndex;
+        }
+        if (minIndex != index)
+        {
+            Swap(minIndex, index);
+            HeapifyDown(minIndex);
+        }
+
+    }
+
+
+    private void HeapifyUp(int index)
+    {
+        if (index == 0) return; // The root of the heap has no parent
+        int parentIndex = (index - 1) / 2;
+
+        if (Heap[parentIndex] > Heap[index])
+        {
+            Swap(parentIndex, index);
+            HeapifyUp(parentIndex);
+        }
+
+    }
+
+    private void Swap(int parentIndex, int index)
+    {
+        int temp = Heap[parentIndex];
+        Heap[parentIndex] = Heap[index];
+        Heap[index] = temp;
+    }
+}
---
+++ Heap Leetcode/Program.cs
@@ -0,0 +1,19 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Hello, World!");
+MinHeap minHeap = new MinHeap();
+
+// Add numbers to the heap
+int[] numbersToAdd = { 5, 3, 8, 1, 2, 7 };
+foreach (var num in numbersToAdd)
+{
+    Console.WriteLine($"Adding: {num}");
+    minHeap.Add(num);
+}
+
+// Pop numbers from the heap and print them
+Console.WriteLine("Popping numbers from the heap in ascending order:");
+while (minHeap.Heap.Count > 0)
+{
+    int min = minHeap.Pop();
+    Console.WriteLine(min);
+}
